d108281e921ebb63e3d807fc86ecbdbe36e9edca
George O <geoorge191@hotmail.com>
2019-10-07 09:43:18 -0500

    Cambio de 30 y 90 dias naturales

---
+++ Code/avisoPrueba/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(265, 36)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Hoy terminó el periodo de prueba de:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(10, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(-1, 128)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(294, 10)
+        Me.Label3.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(-1, -5)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(294, 10)
+        Me.Label4.TabIndex = 4
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 5
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.Coral
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(97, 102)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(100, 23)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Aceptar"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(5, 84)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(91, 13)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Inició en la fecha:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(103, 84)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(39, 13)
+        Me.Label9.TabIndex = 9
+        Me.Label9.Text = "Label9"
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 14400000
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.avisoPrueba.My.Resources.Resources.btn_cerrar
+        Me.PictureBox1.Location = New System.Drawing.Point(221, 6)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(69, 28)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SteelBlue
+        Me.ClientSize = New System.Drawing.Size(292, 133)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(292, 133)
+        Me.MinimumSize = New System.Drawing.Size(292, 133)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+
+End Class
---
+++ Code/avisoPrueba/Form1.vb
@@ -0,0 +1,145 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim tVentanaY As Integer = 133
+    Dim tVentanaX As Integer = 292
+    Dim anombre() As String
+    Dim afechaini() As String
+    Dim afechafin() As String
+    Dim m As Integer
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'animacionventana()
+        'Timer2.Start()
+        'Timer1.Start()
+        Dim nombre As String = ""
+        Dim fecha As Date
+        Dim fechaInicio As String = ""
+        Dim lector As SqlDataReader
+        Dim bandera As Boolean = False
+        Dim n As Integer
+        Try
+            m = 1
+            n = 0
+            Using cnx = New SqlConnection("Data Source=192.168.3.254;Initial Catalog=Vistas;User ID=sa;Password=inventumc762$")
+                cnx.Open()
+                Using cmd As New SqlCommand()
+                    cmd.Connection = cnx
+                    cmd.CommandText = "SET DATEFORMAT dmy   select Nombre, fechaFin, fechaInicio from RH_periodoPrueba where fechaFin>=getdate() -1 and fechaFin<=getdate() + 5 order by fechaFin asc"
+
+
+                    lector = cmd.ExecuteReader
+                    While lector.Read
+                        nombre = CStr(lector(0).ToString)
+                        fecha = CStr(lector(1).ToString)
+                        fechaInicio = CStr(CDate(lector(2).ToString))
+                        Dim numdias As Integer = DateDiff(DateInterval.Day, Today, CDate(fecha))
+
+
+                        If numdias <= 5 And numdias >= 0 Then
+                            'Dim sonido = New System.Media.SoundPlayer
+                            Select Case numdias
+                                Case 0
+                                    Label1.Text = "Hoy terminó el periodo de prueba de:"
+                                Case 1
+                                    Label1.Text = "Falta un dia para que finalize periodo de prueba de:"
+                                Case 2
+                                    Label1.Text = "Faltan dos dias para que finalize el periodo de prueba de:"
+                                Case 3
+                                    Label1.Text = "Faltan tres dias para que finalize el periodo de prueba de:"
+                                Case 4
+                                    Label1.Text = "Faltan cuatro dias para que finalize el periodo de prueba de:"
+                                Case 5
+                                    Label1.Text = "Faltan cinco dias para que finalize el periodo de prueba de:"
+                            End Select
+
+                            ReDim Preserve anombre(n)
+                            ReDim Preserve afechaini(n)
+                            ReDim Preserve afechafin(n)
+                            anombre(n) = nombre
+                            afechaini(n) = fechaInicio
+                            afechafin(n) = Label1.Text
+                            n = n + 1
+
+                            Label2.Text = nombre
+                            Label9.Text = fechaInicio
+                            bandera = True
+                            'sonido = New System.Media.SoundPlayer(My.Application.Info.DirectoryPath + "\Sound_WAV.wav")
+                            'sonido.Play()
+                        Else
+
+                        End If
+                    End While
+                    lector.Close()
+                    If bandera = False Then
+                        Me.Close()
+                    End If
+                End Using
+                cnx.Close()
+            End Using
+            If anombre.Length >= 1 Then
+                otravez(0)
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "HA ocurrido un error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Function animacionventana()
+        Dim desktopSize As Size
+        desktopSize = System.Windows.Forms.SystemInformation.PrimaryMonitorSize
+        Dim height As Integer = desktopSize.Height
+        Dim width As Integer = desktopSize.Width
+        Me.Location = New Point(width - tVentanaX, height - tVentanaY + 180)
+
+        tVentanaY = tVentanaY + 5
+        If tVentanaY >= 354 Then
+            Timer1.Stop()
+        End If
+        Return Nothing
+    End Function
+
+    Function otravez(ByVal veces As Integer)
+        Dim sonido As System.Media.SoundPlayer
+        sonido = New System.Media.SoundPlayer(My.Application.Info.DirectoryPath + "\Sound_WAV.wav")
+        tVentanaY = 133
+        animacionventana()
+        Timer2.Start()
+        Timer1.Start()
+        Label1.Text = afechafin(veces)
+        Label2.Text = anombre(veces)
+        Label9.Text = afechaini(veces)
+
+        sonido.Play()
+
+        Return Nothing
+    End Function
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        animacionventana()
+    End Sub
+
+    Private Sub Label7_Click(sender As System.Object, e As System.EventArgs) Handles Label7.Click
+
+        If anombre.Length > 1 And m <= anombre.Length - 1 Then
+            Me.Visible = True
+            otravez(m)
+            m = m + 1
+        Else
+            Me.Visible = False
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As System.Object, e As System.EventArgs) Handles PictureBox1.Click
+        If anombre.Length > 1 And m <= anombre.Length - 1 Then
+            Me.Visible = True
+            otravez(m)
+            m = m + 1
+        Else
+            Me.Visible = False
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As System.Object, e As System.EventArgs) Handles Timer2.Tick
+        Me.Visible = True
+    End Sub
+End Class
